c52cfff3f31a3a077dc1a9f59bd0cc33342a0fc7
LearnByDoingXW <40288492+LearnByDoingXW@users.noreply.github.com>
2018-11-29 13:02:19 +0800

    Add files via upload

--- src/cn/xben/domain/Customer.java
+++ src/cn/xben/domain/Customer.java
@@ -4,26 +4,32 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Customer {
+	// 该类为crm Customer顾客类,包含信息如下：编号，姓名，性别，出生日期，电话，手机，级别，来源，行业
 
 	private Long cust_id;
 	private String cust_name;
-	private String cust_source;
-	private String cust_industry;
-	private String cust_level;
-	private String cust_linkman;
+	private String cust_gender;
+	private String cust_born;
 	private String cust_phone;
 	private String cust_mobile;
+//	private String cust_level;
+//	private String cust_source;
+//	private String cust_industry;
+	
+	private BaseDict cust_level;//cust_level.dict_id
+	private BaseDict cust_source;
+	private BaseDict cust_industry;
 
-	// 表达一对多关系
+	// Customer与shopping表示一对多关系 用HashSet存储shopping对象
 
-	private Set<LinkMan> linkMens = new HashSet<LinkMan>();
+	private Set<Shopping> shoppings = new HashSet<Shopping>();
 
-	public Set<LinkMan> getLinkMens() {
-		return linkMens;
+	public Set<Shopping> getShoppings() {
+		return shoppings;
 	}
 
-	public void setLinkMens(Set<LinkMan> linkMens) {
-		this.linkMens = linkMens;
+	public void setShoppings(Set<Shopping> shoppings) {
+		this.shoppings = shoppings;
 	}
 
 	public Long getCust_id() {
@@ -42,36 +48,20 @@ public class Customer {
 		this.cust_name = cust_name;
 	}
 
-	public String getCust_source() {
-		return cust_source;
+	public String getCust_gender() {
+		return cust_gender;
 	}
 
-	public void setCust_source(String cust_source) {
-		this.cust_source = cust_source;
+	public void setCust_gender(String cust_gender) {
+		this.cust_gender = cust_gender;
 	}
 
-	public String getCust_industry() {
-		return cust_industry;
+	public String getCust_born() {
+		return cust_born;
 	}
 
-	public void setCust_industry(String cust_industry) {
-		this.cust_industry = cust_industry;
-	}
-
-	public String getCust_level() {
-		return cust_level;
-	}
-
-	public void setCust_level(String cust_level) {
-		this.cust_level = cust_level;
-	}
-
-	public String getCust_linkman() {
-		return cust_linkman;
-	}
-
-	public void setCust_linkman(String cust_linkman) {
-		this.cust_linkman = cust_linkman;
+	public void setCust_born(String cust_born) {
+		this.cust_born = cust_born;
 	}
 
 	public String getCust_phone() {
@@ -90,8 +80,30 @@ public class Customer {
 		this.cust_mobile = cust_mobile;
 	}
 
-	public String toString() {
-		return "Customer [cust_id=" + cust_id + "cust_name=" + cust_name + "]";
+	public BaseDict getCust_level() {
+		return cust_level;
 	}
+
+	public void setCust_level(BaseDict cust_level) {
+		this.cust_level = cust_level;
+	}
+
+	public BaseDict getCust_source() {
+		return cust_source;
+	}
+
+	public void setCust_source(BaseDict cust_source) {
+		this.cust_source = cust_source;
+	}
+
+	public BaseDict getCust_industry() {
+		return cust_industry;
+	}
+
+	public void setCust_industry(BaseDict cust_industry) {
+		this.cust_industry = cust_industry;
+	}
+
+	
 
 }
